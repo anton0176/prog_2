3b7495ba8128d13b36ff3eb5f03a9541dd55acc3
anton0176 <42737675+anton0176@users.noreply.github.com>
2019-09-26 13:17:02 +0200

    testa lite

--- src/Flaggor/Flagga.java
+++ src/Flaggor/Flagga.java
@@ -1,16 +1,56 @@
 package Flaggor;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+
+import static javafx.scene.paint.Color.RED;
 
 public class Flagga extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 
     @Override
     public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
 
+        root.setTop(getTopLabel());
+        root.setBottom(new Label("Bottom"));
+        root.setLeft(new Label("Left"));
+        root.setRight(new Label("Right"));
+        root.setCenter(new Label("Center"));
+
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("Hbox,Pane");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+        public Label getTopLabel()
+    {
+            Label lbl = new Label("Top Label");
+
+
+            lbl.setStyle("-fx-border-style: dotted; -fx-border-width: 0 0 1 0; -fx-background-color:#0000FF;-fx-font-weight:bold;");
+
+            lbl.setPrefHeight(60);
+            lbl.setPrefWidth(100);
+
+
+
+            return lbl;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
