08b6f80f7608b18e6f639371cafffbee8143f73e
anton0176 <42737675+anton0176@users.noreply.github.com>
2019-09-28 11:50:57 +0200

    tog bort några filer

---
+++ src/Flaggor/Flags.java
@@ -0,0 +1,26 @@
+package Flaggor;
+
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+public class Flags extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
